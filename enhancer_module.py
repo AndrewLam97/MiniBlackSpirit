6990cbb350648acd842f23243694861d1240e35c
jhsu0811 <52256871+jhsu0811@users.noreply.github.com>
2020-10-01 16:39:36 -0700

    Create enhancer_module.py

---
+++ enhancer_module.py
@@ -0,0 +1,11 @@
+import random
+
+#Returns Success of Random Roll as a String
+def enhancement_sim_function(inputnum):
+    random.seed()
+    roll_num = random.random()*100
+    if roll_num <= inputnum:
+        return "{:.2f} was rolled. Enchantment Success!".format(roll_num)
+    else:
+        return "{:.2f} was rolled. Enchantment Failed. Better luck next time.".format(roll_num)
+
