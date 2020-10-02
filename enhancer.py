7ca0dad67f9d97af1fc4f0dde9b0554d25052fd6
jhsu0811 <52256871+jhsu0811@users.noreply.github.com>
2020-10-01 18:46:44 -0700

    Changed name of file and function for clarity to

--- enhancer.py
+++ enhancer.py
@@ -1,7 +1,7 @@
 import random
 
 #Returns Success of Random Roll as a String
-def enhancement_sim_function(inputnum):
+def enhancement_sim(inputnum):
     random.seed()
     roll_num = random.random()*100
     if roll_num <= inputnum:
